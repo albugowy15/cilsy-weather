a3dde440eec345fde1fe81562a4b6bfb63f77146
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-10-14 12:19:13 +0700

    feat: add tailwind

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="text-red-500 text-lg font-bold flex items-center justify-center h-screen">
+      Hello world
+    </div>
+  );
+}
