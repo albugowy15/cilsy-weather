056ea0d88c54d87499b946ba0d64dffa0fea5cb7
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-10-16 22:19:59 +0700

    feat: add shadcn-ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 import * as React from "react";
 
 export default function HomePage() {
   return (
     <div className="text-red-500 text-lg font-bold flex items-center justify-center h-screen">
+      <Button>Shadcn ui button</Button>
       Hello world
     </div>
   );
